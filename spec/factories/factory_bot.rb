3167fb08f02382c5ca5f7f6ecee18d574269d80b
Bettenoch <bettenoch254@gmail.com>
2023-11-30 13:36:45 +0300

    Refactor factory bot to reduce block length

--- spec/factories/factory_bot.rb
+++ spec/factories/factory_bot.rb
@@ -1,5 +1,4 @@
-require 'faker'
-
+# user_factory.rb
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
@@ -7,48 +6,64 @@ FactoryBot.define do
     password { 'password123' }
     profile { Faker::Lorem.sentence }
 
-    factory :user_with_categories_and_purchases do
+    trait :with_categories_and_purchases do
       transient do
         categories_count { 5 }
         purchases_count { 10 }
       end
 
       after(:create) do |user, evaluator|
-        create_list(:category, evaluator.categories_count, user:).each do |category|
-          create_list(:purchase, evaluator.purchases_count, author: user).each do |purchase|
-            create(:category_purchase, category:, purchase:)
-          end
-        end
+        create_categories_and_purchases(user, evaluator.categories_count, evaluator.purchases_count)
       end
     end
   end
+end
 
+# category_factory.rb
+FactoryBot.define do
   factory :category do
     name { Faker::Lorem.word }
     icon { Faker::Lorem.word }
     association :user
 
-    factory :category_with_purchases do
+    trait :with_purchases do
       transient do
         purchases_count { 5 }
       end
 
       after(:create) do |category, evaluator|
-        create_list(:purchase, evaluator.purchases_count, author: category.user).each do |purchase|
-          create(:category_purchase, category:, purchase:)
-        end
+        create_purchases(category, evaluator.purchases_count)
       end
     end
   end
+end
 
+# purchase_factory.rb
+FactoryBot.define do
   factory :purchase do
     name { Faker::Lorem.word }
     amount { Faker::Number.decimal(l_digits: 2) }
     association :author, factory: :user
   end
+end
 
+# category_purchase_factory.rb
+FactoryBot.define do
   factory :category_purchase do
     association :category
     association :purchase
+  end
+end
+
+# factories_helper.rb
+def create_categories_and_purchases(user, categories_count, purchases_count)
+  create_list(:category, categories_count, user:).each do |category|
+    create_purchases(category, purchases_count)
+  end
+end
+
+def create_purchases(category, purchases_count)
+  create_list(:purchase, purchases_count, author: category.user).each do |purchase|
+    create(:category_purchase, category:, purchase:)
   end
 end
